46e4e07b9f9430224fc6c9c95383a36b833d7e05
Igor Pavlichenko <pawigor@komcity.ru>
2016-03-30 01:35:47 +1000

    Игра угадай число :)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::io;
 
 fn main(){
-	println!("Hello, Rust!");
+	println!("Угадай число!");
+	
+	println!("Введи число");
+	
+	let mut guess = String::new();
+	
+	io::stdin().read_line(&mut guess)
+		.expect("Не шмогла ");
+	
+	println!("Угадал: {}",guess);
 }
