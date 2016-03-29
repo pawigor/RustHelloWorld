34aa34eb2fdeb806f91328b4cf76efcf8eb4c92f
Igor Pavlichenko <pawigor@komcity.ru>
2016-03-30 02:11:04 +1000

    Игра, угадай число.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main(){
 	println!("Угадай число!");
 	
-	println!("Введи число");
+	let secret_number = rand::thread_rng().gen_range(1,101);
 	
-	let mut guess = String::new();
+	//println!("Секретное число: {}", secret_number);
 	
-	io::stdin().read_line(&mut guess)
-		.expect("Не шмогла ");
-	
-	println!("Угадал: {}",guess);
+	loop{
+		println!("Введи число");
+
+		let mut guess = String::new();
+
+		io::stdin().read_line(&mut guess)
+			.expect("Не шмогла ");
+
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num)=>num,
+			Err(_)=>continue,
+		};
+
+		println!("Угадал: {}",guess);
+
+		match guess.cmp(&secret_number) {
+			Ordering::Less    => println!("маленький!"),
+			Ordering::Greater => println!("большой!"),
+			Ordering::Equal   => {
+					println!("победа!");
+				 	break;
+				},
+		}
+	}
 }
